f528a61620ec2fb07765e8332c757b905f51da21
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-01-24 16:14:14 -0600

    gameroom from function => component

--- src/components/Room.js
+++ src/components/Room.js
@@ -3,25 +3,27 @@ import WaitingRoom from './Waiting-room'
 import GameStarted from './Game-started'
 import GameEnded from './Game-ended'
 
-function GameRoom() {
-    return (
-        <div className='gameScreen'>
-            <div className='gameScreenLeft'>
-                <p className='playersLabel'>
-                    players:
-                    {/* Get list of players (API call) */}
-                </p>
-                <div className='playersList'>
-                    <li>
-                        ahaha
-                    </li>
+class GameRoom extends React.Component {
+    render() {
+        return (
+            <div className='gameScreen'>
+                <div className='gameScreenLeft'>
+                    <p className='playersLabel'>
+                        players:
+                        {/* Get list of players (API call) */}
+                    </p>
+                    <div className='playersList'>
+                        <li>
+                            ahaha
+                        </li>
+                    </div>
+                </div>
+                <div className='gameScreenRight'>
+                    <WaitingRoom />
                 </div>
             </div>
-            <div className='gameScreenRight'>
-                <WaitingRoom />
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default GameRoom;
