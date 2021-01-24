51e9716aaac0f3177136312f37c4a7259606a0cf
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-01-24 12:11:15 -0600

    ¯\_(ツ)_/¯

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
         <div>
             <p style={{ height: '5vh', textAlign: 'right', marginRight: '10px' }}><a href='https://github.com/fibanneacci/liar' target='blank' rel='noreferrer'>built</a> by <a href='https://twitter.com/fibanneacci' target='_blank' rel='noreferrer'>@fibanneacci</a>.</p>
             <div className='container' style={{ height: '90vh' }}>
-                <h1 style={{ marginTop: '-10px' }}>who's the liar? 👀</h1>
+                <h1 style={{ marginTop: '-20px' }}>who's the liar? 👀</h1>
                 <button class='block' style={{ marginTop: '-15px', marginLeft: 'auto', marginRight: 'auto' }}>create room</button>
             </div>
         </div>
