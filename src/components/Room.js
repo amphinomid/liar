ecb413bd1fb65db562ba1eced8bb50ec8f2642c6
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-01-26 22:28:11 -0600

    players list display

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,21 +1,36 @@
 import React from 'react'
+import firebase from '../firebase.js'
 import WaitingRoom from './Waiting-room'
 import GameStarted from './Game-started'
 import GameEnded from './Game-ended'
 
-class GameRoom extends React.Component {
+const id = window.location.pathname.substring(6)
+const game = firebase.database().ref('games').child(id)
+game.child('players').on('child_changed', function(snapshot) {
+    var changed = snapshot.val()
+    console.log('The updated value is ' + changed)
+})
+
+class Room extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            players: [], // define in terms of database
+            stage: 0
+        }
+    }
+
     render() {
         return (
             <div className='gameScreen'>
                 <div className='gameScreenLeft'>
                     <p className='playersLabel'>
                         players:
-                        {/* Get list of players (API call) */}
                     </p>
                     <div className='playersList'>
-                        <li>
-                            ahaha
-                        </li>
+                        {this.state.players.map((player) =>
+                            <li key={player}>{player}</li>
+                        )}
                     </div>
                 </div>
                 <div className='gameScreenRight'>
@@ -26,4 +41,4 @@ class GameRoom extends React.Component {
     }
 }
 
-export default GameRoom;
+export default Room;
