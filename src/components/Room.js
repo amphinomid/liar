6aafb26d641b1fc71c3c307bb1ae45f3e08956bd
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-01-28 22:21:42 -0600

    navigate game room

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import firebase from '../firebase.js'
-import WaitingRoom from './Waiting-room'
-import GameStarted from './Game-started'
-import GameEnded from './Game-ended'
 
 class Room extends React.Component {
     constructor(props) {
@@ -11,23 +8,21 @@ class Room extends React.Component {
             players: [],
             stage: 0
         }
+        this.updateStage = this.updateStage.bind(this)
+    }
+
+    updateStage() {
+        this.setState({
+            stage: (this.state.stage + 1) % 3
+        })
     }
 
     // Based off https://css-tricks.com/building-a-real-time-chat-app-with-react-and-firebase/
     async componentDidMount() {
         const id = window.location.pathname.substring(6)
         const players = firebase.database().ref('games').child(id).child('players')
-        // players.once('value', snapshot => {
-        //     snapshot.forEach((player) => {
-        //         var updatedPlayers = this.state.players
-        //         updatedPlayers.push(player.child('name').val())
-        //         this.setState({
-        //             players: updatedPlayers
-        //         })
-        //     })
-        // })
         players.on('child_added', player => {
-            var updatedPlayers = this.state.players
+            let updatedPlayers = this.state.players
             updatedPlayers.push(player.child('name').val())
             this.setState({
                 players: updatedPlayers
@@ -36,6 +31,35 @@ class Room extends React.Component {
     }
 
     render() {
+        let gameDisplay
+        let stage = this.state.stage
+        if (stage % 3 == 0) {
+            { /* Waiting room */ }
+            gameDisplay = <div>
+                <p className='linkLabel'>send your friends this link:</p>
+                <p className='link'>{'liar-ga.me' + window.location.pathname.replace('room', 'enter')}</p>
+                <button className='block' onClick={this.updateStage} style={{ marginTop: '20px', marginLeft: 'auto', marginRight: 'auto' }}>start</button>
+            </div>
+        } else if (stage % 3 == 1) {
+            { /* Game started */ }
+            gameDisplay = <div>
+                <div className='promptLabel'>
+                    {/* Get category, word, liar, etc. (API call) */}
+                    category is FOOD<br />
+                    word is APPLE / you are the LIAR
+                </div>
+                <button className='block' onClick={this.updateStage} style={{ marginTop: '20px', marginLeft: 'auto', marginRight: 'auto' }}>end</button>
+            </div>
+        } else {
+            { /* Game ended */ }
+            gameDisplay = <div>
+                <div className='liarLabel'>
+                    {/* Get liar (API call) */}
+                    ahaha was the liar!
+                </div>
+                <button className='block' onClick={this.updateStage} style={{ marginTop: '20px', marginLeft: 'auto', marginRight: 'auto' }}>play again</button>
+            </div>
+        }
         return (
             <div className='gameScreen'>
                 <div className='gameScreenLeft'>
@@ -49,7 +73,7 @@ class Room extends React.Component {
                     </div>
                 </div>
                 <div className='gameScreenRight'>
-                    <WaitingRoom />
+                    {gameDisplay}
                 </div>
             </div>
         );
