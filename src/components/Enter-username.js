c060e0744ef45564cb5ea553e8f8c7e9a67b40ba
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-01-29 18:46:42 -0600

    alphanumeric-only usernames

--- src/components/Enter-username.js
+++ src/components/Enter-username.js
@@ -6,33 +6,38 @@ var rand = require('randomstring')
 function EnterUsername() {
     const hist = useHistory()
     function Submit() {
-        const url = window.location.pathname
-        const games = firebase.database().ref('games')
         const name = document.getElementById('username').value
-        // If URL doesn't have id, create room
-        if (url.length == 6) {
-            const id = rand.generate(7)
-            const wordset = {
-                category: "",
-                word: ""
+        // If contains non-alphanumeric
+        if (/[^0-9A-Za-z]/.test(name)) {
+            document.getElementById('usernameValidationLabel').innerHTML = 'please enter alphanumeric only.'
+        } else {
+            const url = window.location.pathname
+            const games = firebase.database().ref('games')
+            // If URL doesn't have id, create room
+            if (url.length == 6) {
+                const id = rand.generate(7)
+                const wordset = {
+                    category: "",
+                    word: ""
+                }
+                const game = {
+                    liar: "",
+                    players: [
+                        name
+                    ],
+                    stage: 0,
+                    wordset: wordset
+                }
+                games.child(id).set(game)
+                hist.replace('/room/' + id + '&name=' + name)
             }
-            const game = {
-                liar: "",
-                players: [
-                    name
-                ],
-                stage: 0,
-                wordset: wordset
+            // If URL has id, join existing room
+            else {
+                const id = url.substring(7, 14)
+                const player = document.getElementById('username').value
+                games.child(id).child('players').push(player)
+                hist.replace('/room/' + id + '&name=' + name)
             }
-            games.child(id).set(game)
-            hist.replace('/room/' + id + '&name=' + name)
-        }
-        // If URL has id, join existing room
-        else {
-            const id = url.substring(7, 14)
-            const player = document.getElementById('username').value
-            games.child(id).child('players').push(player)
-            hist.replace('/room/' + id + '&name=' + name)
         }
     }
 
@@ -41,6 +46,7 @@ function EnterUsername() {
             <label className='usernameLabel' htmlFor='username'>username:</label> {/*check for uniqueness, or just add a number to the end if not unique*/}
             <input className='usernameInput' type='text' id='username' />
             <button className='block' onClick={Submit} style={{ marginLeft: 'auto', marginRight: 'auto' }}>enter</button>
+            <p className='usernameValidationLabel' id='usernameValidationLabel'>(alphanumeric only)</p>
         </div>
     );
 }
